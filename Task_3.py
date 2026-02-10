6400379bc9c018a78839b8a3969270aafba8fffe
pruch-de-dull <pruch.de.dull@gmail.com>
2026-02-10 10:23:40 +0200

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -8,7 +8,7 @@ def normalize_phone(phone_number):
     elif phone_number_cleaned.startswith('380'):
         new_phone = f'+{phone_number_cleaned}' 
     else:
-        new_phone = f('+38{phone_number_cleaned}') 
+        new_phone = f"+38{phone_number_cleaned}"
     return new_phone
 f = normalize_phone('    +38(050)123-32-34')
 print(f)
