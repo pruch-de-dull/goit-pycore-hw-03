128d721e2658cc3f7bbbb0ee9b1f20eb7093351f
pruch-de-dull <pruch.de.dull@gmail.com>
2026-02-19 18:08:52 +0200

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,19 @@
 from datetime import datetime
 
-def get_days_from_today(date):
-    date_n = datetime.strptime(date, '%Y-%m-%d')
-    now = datetime.now ()
+
+def get_days_from_today(date: str) -> int | None:
+  
+    try:
+        date_n = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print(f"Помилка: дата '{date}' не відповідає формату 'РРРР-ММ-ДД'.")
+        return None
+        
+    now = datetime.now()
     difference = now - date_n
     return difference.days
-f =  get_days_from_today ('2020-9-10')
-print(f)
+
+
+if __name__ == "__main__":
+    f = get_days_from_today('2020-09-10')
+    print(f"Кількість днів: {f}")
