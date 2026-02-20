2ae1b5884929936b32268c2e66df6005fee8150e
pruch-de-dull <pruch.de.dull@gmail.com>
2026-02-20 09:46:35 +0200

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,13 +1,17 @@
 import random
-def get_numbers_ticket(min, max, quantity):
+
+
+def get_numbers_ticket(min_num: int, max_num: int, quantity: int) -> list[int]:
     if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
-        return 'Щось не так'
+        return []
+        
     results_set = set()
     while len(results_set) < quantity:
         results_set.add(random.randrange(min, max + 1))
 
-    result_list =  sorted(list(results_set))
-    return result_list
+    return sorted(list(results_set))
 
-f = get_numbers_ticket(2, 28, 50)
-print(f)
+
+if __name__ == "__main__":
+    f = get_numbers_ticket(2, 28, 50)
+    print(f)
