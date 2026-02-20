c59ef4ac4ebd07b93f5f0a9bc18bace18a935521
pruch-de-dull <pruch.de.dull@gmail.com>
2026-02-20 09:49:08 +0200

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,6 +1,9 @@
 import re
-def normalize_phone(phone_number):
+
+
+def normalize_phone(phone_number: str) -> str:
     phone_number_cleaned = re.sub(r'[^+\d]', '', phone_number.strip())
+    
     if phone_number_cleaned.startswith('+38'):
         new_phone = phone_number_cleaned
     elif phone_number_cleaned.startswith('+'):
@@ -9,6 +12,10 @@ def normalize_phone(phone_number):
         new_phone = f'+{phone_number_cleaned}' 
     else:
         new_phone = f"+38{phone_number_cleaned}"
+        
     return new_phone
-f = normalize_phone('    +38(050)123-32-34')
-print(f)
+    
+
+if __name__ == "__main__":
+    f = normalize_phone('    +38(050)123-32-34')
+    print(f)
